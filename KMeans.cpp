20f5ad4389cf6925f1c13f771c4e9931b907f929
Scott Lee <scott.lee@ucdenver.edu>
2015-10-14 23:31:17 -0600

    KMeans implemented, awating testing

--- KMeans.cpp
+++ KMeans.cpp
@@ -12,8 +12,9 @@ namespace Clustering
         clusterArray= new Cluster[k];//create array of k clusters
         clusterArray[0].setReleasePoints(true);//set the master to release it's points
         this->k=k;
-        scorediff=SCORE_DIFF_THRESHOLD+1;
+        scoreDiff =SCORE_DIFF_THRESHOLD+1;
         score=1000.0;
+        numberOfIterations=0;
 
         is>>clusterArray[0];//load teh dataz into master cluster
         clusterArray[0].pickPoints(k,initialCentroids);//get initial centroids from the dataset
@@ -23,36 +24,72 @@ namespace Clustering
         }
     }
 
-    double KMeans::computeClusteringScore()
+    void KMeans::computeClusteringScore()
     {
-        double intra=0,inter=0;
+        double intra=0,inter=0,oldscore;
         for(int x=0;x<k;x++)
         {
             intra+=(clusterArray[x].intraClusterDistance())/k;//calc mean intracluster distance
         }
         for(int x=0;x<k-1;x++)
             for(int y=x+1;y<k;y++)
-            {
                 inter+=(interClusterDistance(clusterArray[x],clusterArray[y])/k);//calc mean intercluster distance
-            }
-        return intra/inter;//return the ratio of the two
+
+        oldscore=score;
+        score= intra/inter;//set score to the ratio of the two
+        scoreDiff =(score+oldscore)/2;
     }
 
+    void KMeans::recalculateInvalidCentroids()
+    {
+        for(int x=0;x<k;x++)
+        {
+            if(!clusterArray[x].centrodValid())
+                clusterArray[x].computeCentroid();
+        }
+    }
     void KMeans::iterateOnce()
     {
         double tempDistance=0;
-        for(unsigned long int x=0;x<k;x++)//go through each cluster
+        bool nextPoint=false;
+        for(int containingCluster =0; containingCluster <k; containingCluster++)//go through each cluster
         {
-            for(unsigned long int y;y<clusterArray[x].getSize();y++)//test each of it's points
+            for(unsigned long int currentPoint=0; currentPoint <clusterArray[containingCluster].getSize(); currentPoint++)//and each of it's points
             {
-                tempDistance=clusterArray[x][y]->distanceTo(clusterArray[x].getCentroid());
-                for(unsigned long int z=x+1;z<k;z++)//against the other points
+                tempDistance=clusterArray[containingCluster][currentPoint]->distanceTo(clusterArray[containingCluster].getCentroid());//get distance to current centroid
+                for(int targetCluster = containingCluster +1; targetCluster <k; targetCluster++)//test against the other clusters
                 {
-                    if(clusterArray[x][y]->distanceTo(clusterArray[z].getCentroid())<tempDistance)
-
-
+                    if(clusterArray[containingCluster][currentPoint]->distanceTo(clusterArray[targetCluster].getCentroid())<tempDistance)//if it's closer to the target cluster
+                    {
+                        Cluster::Move(clusterArray[containingCluster][currentPoint], &(clusterArray[containingCluster]), &(clusterArray[targetCluster]));//move it
+                        currentPoint--;
+                        break;
+                    }
                 }
             }
         }
+        recalculateInvalidCentroids();
+        computeClusteringScore();
+        std::cout<<"Iteration: "<<numberOfIterations++<<" Score: "<<score<<" ScoreDiff: "<< scoreDiff <<std::endl;
+    }
+
+
+    void KMeans::iterateUntill(unsigned long maxIterations)
+    {
+        bool limit;
+        unsigned long int startIteration=numberOfIterations;
+        maxIterations==0?(limit=true):(limit=false);//determine if the function is supposed to stop after a number of iterations
+        while(scoreDiff>SCORE_DIFF_THRESHOLD)
+        {
+            iterateOnce();
+            if(limit&&numberOfIterations-startIteration>maxIterations)//end before threshold if at max iterations
+                return;
+        }
+    }
+
+    void KMeans::outputPoints(std::ostream &os)
+    {
+        for(int x=0;x<k;x++)
+            os<<(clusterArray[x]);
     }
 }
--- KMeans.h
+++ KMeans.h
@@ -12,12 +12,16 @@ namespace Clustering
         static constexpr double SCORE_DIFF_THRESHOLD=2;
     private:
         Cluster *clusterArray;
-        double score,scorediff;
+        double score, scoreDiff;
         int k;
+        unsigned long int numberOfIterations;
     public:
         KMeans(int k,std::istream &is);
-        double computeClusteringScore();
+        void computeClusteringScore();
+        void recalculateInvalidCentroids();
         void iterateOnce();
+        void iterateUntill(unsigned long int);//iterates until scorediff below threshold, or x times(0 for infinite iterations)
+        void outputPoints(std::ostream& os);
     };
 }
 
